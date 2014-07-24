bc244a5fdd7cb1ed51a279db91959123a9e24b57
Martin Hand <martin@mdhmotors.com>
2014-07-23 18:25:45 -0700

    changed Model: to Part Number: to prevent customer confusion on front end

--- catalog/language/english/checkout/cart.php
+++ catalog/language/english/checkout/cart.php
@@ -36,7 +36,7 @@ $_['text_year']              = 'year';
 // Column
 $_['column_image']           = 'Image';
 $_['column_name']            = 'Product Name';
-$_['column_model']           = 'Model';
+$_['column_model']           = 'Part Number:';
 $_['column_quantity']        = 'Quantity';
 $_['column_price']           = 'Unit Price';
 $_['column_total']           = 'Total';
